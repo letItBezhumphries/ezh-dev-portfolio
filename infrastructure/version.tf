62649e342d81ac39d2b342f96bf6b295853e7100
Eric Humphries <ezhumphries@gmail.com>
2023-09-26 12:54:49 -0700

    changed destroy job

--- infrastructure/version.tf
+++ infrastructure/version.tf
@@ -1,24 +1,4 @@
-# terraform {
-#   required_version = "~> 1.5.2"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.0"
-#     }
-#     # cloudflare = {
-#     #   source  = "cloudflare/cloudflare"
-#     #   version = "2.19.2"
-#     # }
-#     cloudflare = {
-#       source  = "cloudflare/cloudflare"
-#       version = "~> 4.0"
-#     }
-#     random = {
-#       source  = "hashicorp/random"
-#       version = "3.1.0"
-#     }
-#   }
-# }
+
 
 terraform {
   required_providers {
