1e0516aa931a88a16fdcc85205f689ba364d39e2
Eric Humphries <ezhumphries@gmail.com>
2024-07-31 20:46:21 -0700

    tried to fix the responsiveness issue with the images and the macbook png overlap

--- src/components/Laptop.js
+++ src/components/Laptop.js
@@ -4,9 +4,9 @@ import './Laptop.scss';
 const Laptop = ({ image, bg }) => {
   return (
     <div className="laptop-bg">
-      <div className="image-bg">
-        <Image as={'div'} className="app-image" src={image.src} alt={image.alt} />
-      </div>
+      {/* <div className="image-bg"> */}
+      <Image as={'div'} className="app-image" src={image.src} alt={image.alt} />
+      {/* </div> */}
     </div>
   );
 };
